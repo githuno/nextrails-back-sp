48e34b52eb5dda72114a0b007c067c5ffba58c68
githuno <clas.warehouse@gmail.com>
2023-12-19 22:49:13 +0900

    MOD FUNC: video_up

--- app/controllers/gyve/v1/videos_controller.rb
+++ app/controllers/gyve/v1/videos_controller.rb
@@ -1,62 +1,52 @@
 class Gyve::V1::VideosController < ApplicationController
-    require 'aws-sdk-s3'
-    
+    before_action :set_base_name, only: [:create, :pre_create]
+
     def pre_create
         obj = ImageObject.find_or_create_by(id: params[:object_id], user_id: params[:user_id])
         usr_id = params[:user_id]
-        base_name = params[:image_path].split("/")[-1].sub(/\.mp4$/, '')
-        image_path = "#{ENV['S3_PUBLIC_URL']}/#{obj.id}/#{base_name}.png"
+        image_path = "#{ENV['S3_PUBLIC_URL']}/#{obj.id}/#{@base_name}.png"
 
         begin
-            Image.create(object_id: obj.id, image_path: image_path, updated_by: usr_id)
-            s3_key = "#{obj.id}/#{base_name}.mp4"
-            presigned_url = generate_presigned_url(s3_key)
-            render json: { 'msg' => 'success', 'result' => [{ 'id' => base_name, 'path' => presigned_url }] }
+            image = Image.create(object_id: obj.id, image_path: image_path, updated_by: usr_id)
+            s3_key = "#{obj.id}/#{@base_name}.mp4"
+            presigned_url = image.generate_presigned_url(s3_key)
+            render json: { 'msg' => 'success', 'result' => [{ 'id' => @base_name, 'path' => presigned_url }] }
         rescue StandardError => e
-            Rails.logger.error "Error generating presigned URL: #{e}"
+            log_error(e)
             render json: { 'detail' => 'Internal Server Error' }, status: :internal_server_error
         end
     end
 
     def create
-        image_base_path = params[:image_path].split("/")[-1].sub(/\.mp4\?.*$/, '.png')
         obj_id = params[:object_id]
-        image_path = "#{ENV['S3_PUBLIC_URL']}/#{obj_id}/#{image_base_path}"
+        image_path = "#{ENV['S3_PUBLIC_URL']}/#{obj_id}/#{@base_name}.png"
         begin
             sample_upload(image_path)
-            render json: { 'msg' => 'Video uploaded successfully', 'result' => [{ 'id' => image_base_path, 'path' => image_path }] }   
+            render json: { 'msg' => 'Video uploaded successfully', 'result' => [{ 'id' => @base_name, 'path' => image_path }] }   
         rescue StandardError => e
-            Rails.logger.error "Error video uploading process: #{e}"
+            log_error(e)
             render json: { 'detail' => 'Internal Server Error' }, status: :internal_server_error
         end
     end
     
     private
 
-    def sample_upload(image_path)
-        obj_id = image_path.split('/')[-2]
-        base_name = File.basename(image_path, '.*')
-
-        # png file
-        png_path = Rails.root.join('public', 'template.png')
-        png_template = File.read(png_path)
-        png_name = "#{base_name}.png"
-        png_key = "#{obj_id}/#{png_name}"
-
-        image = Image.find_by(image_path: image_path)
-        raise StandardError, 'Image not found' unless image
-        image.file.attach(io: StringIO.new(png_template), key: png_key, filename: png_name, content_type: 'image/png')
-        image.save!
+    def set_base_name
+        @base_name = params[:image_path].split('/').last.sub(/\.[^.]+\z/, '')
     end
 
-    def generate_presigned_url(key)
-        s3 = Aws::S3::Resource.new(
-            region: ENV['AWS_REGION'], 
-            endpoint: ENV['AWS_ENDPOINT'], 
-            access_key_id: ENV['AWS_ACCESS_KEY_ID'], 
-            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-        )
-        obj = s3.bucket(ENV['S3_BUCKET']).object(key)
-        obj.presigned_url(:put, expires_in: 3600)
+    def sample_upload(image_path)
+        obj_id = image_path.split('/')[-2]
+
+        # png file
+        png_template = File.read(ENV['TEMPLATE_PNG_PATH'])
+        png_name = "#{@base_name}.png"
+        png_key = "#{obj_id}/#{png_name}"
+        image = Image.find_by(image_path: image_path)
+        image.file.attach(io: StringIO.new(png_template), key: png_key, filename: png_name, content_type: 'image/png')
+    end
+
+    def log_error(e)
+        Rails.logger.error "#{e.class} (#{e.message}):\n  #{e.backtrace.join("\n  ")}"
     end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,23 +2,39 @@ class Image < ApplicationRecord
   belongs_to :object, class_name: 'ImageObject', foreign_key: 'object_id'
   has_one_attached :file
   has_one_attached :html_file
-  
-  
+
+  # for Video------------------------------------------------------------------>
+  VIDEO_ID_LENGTH = 36 # uuid
+
+  def generate_presigned_url(key)
+    s3 = s3_resource
+    obj = s3.bucket(ENV['S3_BUCKET']).object(key)
+    obj.presigned_url(:put, expires_in: 3600)
+  end
+
   def delete_related_files
-    s3 = Aws::S3::Resource.new(
-      region: ENV['AWS_REGION'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      endpoint: ENV['AWS_ENDPOINT']
-    )
-  
-    base_name = self.image_path.gsub(ENV['S3_PUBLIC_URL'], '').chomp(File.extname(self.image_path))
-    if base_name.length == 36
-      extensions = [".html", ".mp4"]
-      extensions.each do |extension|
-        key = "#{base_name}#{extension}"
-        s3.bucket(ENV['S3_BUCKET']).object(key).delete
-      end
+    s3 = s3_resource
+
+    base_name = file.key.gsub(ENV['S3_PUBLIC_URL'], '').chomp(File.extname(file.key))
+    return unless base_name.length == VIDEO_ID_LENGTH
+
+    extensions = ['.html', '.mp4']
+    extensions.each do |extension|
+      key = "#{base_name}#{extension}"
+      s3.bucket(ENV['S3_BUCKET']).object(key).delete
     end
   end
+
+  private
+
+  def s3_resource
+    require 'aws-sdk-s3'
+    return Aws::S3::Resource.new(
+      region: ENV['AWS_REGION'],
+      access_key_id: ENV['AWS_ACCESS_KEY'],
+      secret_access_key: ENV['AWS_SECRET_KEY'],
+      endpoint: ENV['AWS_ENDPOINT']
+    )
+  end
+  # <------------------------------------------------------------------for Video
 end
