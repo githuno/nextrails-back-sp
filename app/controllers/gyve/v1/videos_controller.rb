66879c85226bbf52296098c3531d1322641eb0a2
githuno <clas.warehouse@gmail.com>
2023-12-19 18:57:56 +0900

    MOD FUNC: video_up

--- app/controllers/gyve/v1/videos_controller.rb
+++ app/controllers/gyve/v1/videos_controller.rb
@@ -4,7 +4,7 @@ class Gyve::V1::VideosController < ApplicationController
     def pre_create
         obj = ImageObject.find_or_create_by(id: params[:object_id], user_id: params[:user_id])
         usr_id = params[:user_id]
-        base_name = params[:image_path].split("/")[-1]
+        base_name = params[:image_path].split("/")[-1].sub(/\.mp4$/, '')
         image_path = "#{ENV['S3_PUBLIC_URL']}/#{obj.id}/#{base_name}.png"
 
         begin
@@ -36,7 +36,7 @@ class Gyve::V1::VideosController < ApplicationController
     def template_Image_and_Html_upload(image_path)
         obj_id = image_path.split('/')[-2]
         base_name = File.basename(image_path, '.*')
-        video_path = image_path.gsub(/\.png$/, '.mp4')
+        video_path = image_path.sub(/\.png$/, '.mp4')
 
         # image file
         png_path = Rails.root.join('public', 'template.png')
@@ -47,7 +47,7 @@ class Gyve::V1::VideosController < ApplicationController
         # HTML file
         html_path = Rails.root.join('public', 'template.html')
         html_template = File.read(html_path)
-        html_content = html_template.gsub('{url}', video_path)
+        html_content = html_template.sub('{url}', video_path)
         html_name = "#{base_name}.html"
         html_key = "#{obj_id}/#{html_name}"
         
