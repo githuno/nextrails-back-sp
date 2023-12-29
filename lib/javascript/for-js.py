117d7301d94c5ee3a34c3161123b93eaeadece52
githuno <clas.warehouse@gmail.com>
2023-12-29 18:45:09 +0900

    js

--- lib/javascript/for-js.py
+++ lib/javascript/for-js.py
@@ -1,8 +1,17 @@
-import subprocess
+import execjs
 
-# JavaScriptファイルの内容を読み込む
-with open('/opt/check-module.js', 'r') as js_file:
-   js_code = js_file.read()
+# JavaScriptエンジンを初期化
+check = execjs.compile(open('/opt/check-module.js').read())
 
-# Node.jsでJavaScriptコードを実行する
-subprocess.call(['node', '-e', js_code])
+# JavaScript関数を呼び出す
+result = check.call()
+
+
+# import subprocess
+
+# # JavaScriptファイルの内容を読み込む
+# with open('/opt/check-module.js', 'r') as js_file:
+#    js_code = js_file.read()
+
+# # Node.jsでJavaScriptコードを実行する
+# subprocess.call(['node', '-e', js_code])
