61973ddede9f9eeb6792c6f00eff1e042ced199d
githuno <clas.warehouse@gmail.com>
2023-12-31 03:36:11 +0900

    pumaでメモリ上限設定

--- config/puma.rb
+++ config/puma.rb
@@ -38,23 +38,23 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # process behavior so workers use less memory.
 #
 
-# -----------------------------------------render用に追記
-preload_app! # 元々コメントアウトされていた
+# -----------------------------------------render用に追記 起動コマンドに追加（bundle exec puma -C config/puma.rb;）
+# preload_app! # 元々コメントアウトされていた
 
 
-before_fork do
-    ActiveRecord::Base.connection_pool.disconnect!
-end
+# before_fork do
+#     ActiveRecord::Base.connection_pool.disconnect!
+# end
 
-on_worker_boot do
-    ActiveRecord::Base.establish_connection
-end
+# on_worker_boot do
+#     ActiveRecord::Base.establish_connection
+# end
 
-# ワーカープロセスごとのメモリ上限 (MB) の設定
-worker_memory_limit_mb = Integer(ENV['WORKER_MEMORY_LIMIT_MB'] || 512)
-before_fork do |server|
-    server.config[:worker_memory_limit] = worker_memory_limit_mb
-end
+# # ワーカープロセスごとのメモリ上限 (MB) の設定
+# worker_memory_limit_mb = Integer(ENV['WORKER_MEMORY_LIMIT_MB'] || 512)
+# before_fork do |server|
+#     server.config[:worker_memory_limit] = worker_memory_limit_mb
+# end
 # -----------------------------------------render用に追記
 
 # Allow puma to be restarted by `bin/rails restart` command.
