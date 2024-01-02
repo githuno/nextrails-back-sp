91b7429966ba686288ae8245dd91a6e96e54fb51
githuno <clas.warehouse@gmail.com>
2024-01-02 18:42:01 +0900

    docekrデプロイ用ディレクトリ追加

--- .deploy/production/entrypoint.sh
+++ .deploy/production/entrypoint.sh
@@ -4,6 +4,10 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+# Gemとbundlerのバージョンを合わせる
+gem update --system
+gem install bundler -v 2.5.3
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 bundle config set --local without 'development test' # 調べる
 bundle install
