f87ca9c69ba203a3e7cd751885ffa1fdb82bb054
githuno <clas.warehouse@gmail.com>
2024-01-03 14:18:48 +0900

    --bind 0.0.0.0を削除

--- .deploy/production/entrypoint.sh
+++ .deploy/production/entrypoint.sh
@@ -4,17 +4,10 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-# # cockroachdb：https://www.cockroachlabs.com/docs/v23.1/build-a-ruby-app-with-cockroachdb-activerecord
-# git clone https://github.com/cockroachlabs/example-app-ruby-activerecord
-# cd example-app-ruby-activerecord
-# which libpq
-# bundle config --local build.pg --with-opt-dir=/usr/local/opt/libpq
-# cd /app
-
 # Gemとbundlerのバージョンを合わせる
 gem install bundler -v 2.5.3
 
-bundle lock --add-platform x86_64-linux;
+# bundle lock --add-platform x86_64-linux;
 
 # ffiのキャッシュを有効化
 bundle config set --local BUNDLE_CACHE__FFI true
